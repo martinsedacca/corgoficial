5a97acf82d7dce612b7d3c87bba9c6ec39b36a41
martinsedacca <m.sedacca@gmail.com>
2025-09-20 12:37:05 -0300

    Fix useEffect import error in PrescriptionHistory

--- src/components/PrescriptionHistory.tsx
+++ src/components/PrescriptionHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useData } from '../contexts/DataContext';
 import { Prescription } from '../types';
 import { PrescriptionForm } from './PrescriptionForm';
