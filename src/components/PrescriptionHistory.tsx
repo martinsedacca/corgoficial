b9d1b17f6f678f0dd140162edff3b5e8f30be5e3
martinsedacca <m.sedacca@gmail.com>
2025-09-22 07:50:17 -0300

    Fix social work field alignment

--- src/components/PrescriptionHistory.tsx
+++ src/components/PrescriptionHistory.tsx
@@ -376,7 +376,7 @@ export default function PrescriptionHistory({ onViewPrescription, onEditPrescrip
         </div>
         
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
-          <div className="lg:col-span-1">
+          <div>
             <DateRangePicker
               startDate={filterDateFrom}
               endDate={filterDateTo}
